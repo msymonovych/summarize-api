0e5f5ccaca1fbfc7890cd413439aa75c30de9e14
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-06-26 23:46:45 +0300

    Add max and min length for summarizer and uvicorn

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from langchain_huggingface import HuggingFacePipeline
 
@@ -6,6 +7,10 @@ app = FastAPI()
 summarizer = HuggingFacePipeline.from_model_id(
     model_id="facebook/bart-large-cnn",
     task="summarization",
+    pipeline_kwargs={
+        "max_length": 50,
+        "min_length": 20
+    }
 )
 
 
@@ -15,3 +20,6 @@ async def summarize(request: Request):
     text = data.get("text", "")
     summary = await summarizer.ainvoke(text)
     return {"summary": summary}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
